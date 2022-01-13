f60ab4f019ee5a4f32dc22ae493facf77a4ff255
BIGBEASTISHANK <pranjal3211p@gmail.com>
2022-01-13 12:23:30 +0530

    Fixed image tag

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -20,16 +20,16 @@ export default function Footer() {
 
             <Center alignItems={'center'} textAlign={'center'} className='img'>
                 <Flex>
-                    <Link className="links" href="https://github.com/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "github.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="mailto:pranjal3211p@gmail.com" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "email.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://instagram.com/opishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "instagram.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://open.spotify.com/user/n37h63t5acey8z9kqu3dwjp1l" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "spotify.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://discord.gg/WdeHDmwKFn" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "discord.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://snapchat.com/add/opishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "snapchat.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://steamcommunity.com/id/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "steam.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://twitter.com/opishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "twitter.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://www.youtube.com/channel/UCw6lNThNWxwz1cz5rvR1Rdw" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "youtube.webp"} height={icon_height} width={icon_width}></Image></Link>
-                    <Link className="links" href="https://fiverr.com/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image src={s_media_path + "fiverr.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://github.com/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "github.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="mailto:pranjal3211p@gmail.com" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "email.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://instagram.com/opishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "instagram.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://open.spotify.com/user/n37h63t5acey8z9kqu3dwjp1l" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "spotify.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://discord.gg/WdeHDmwKFn" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "discord.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://snapchat.com/add/opishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "snapchat.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://steamcommunity.com/id/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "steam.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://twitter.com/opishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "twitter.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://www.youtube.com/channel/UCw6lNThNWxwz1cz5rvR1Rdw" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "youtube.webp"} height={icon_height} width={icon_width}></Image></Link>
+                    <Link className="links" href="https://fiverr.com/bigbeastishank" isExternal m={'2px'} mt={'8px'}><Image alt="" src={s_media_path + "fiverr.webp"} height={icon_height} width={icon_width}></Image></Link>
                 </Flex>
             </Center>
         </>
