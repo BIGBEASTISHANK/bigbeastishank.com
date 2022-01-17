93afffb1c51fb803db95c001e37606e047aad193
BIGBEASTISHANK <pranjal3211p@gmail.com>
2022-01-17 14:13:49 +0530

    (update): Blog

---
+++ src/blogPost/second post.md
@@ -0,0 +1,74 @@
+---
+title: "CBSE term 2 syllabus of class 10"
+description: "As you know term 2 is comming in end or march or start of april. So here is the syllabus of term 2 class 10."
+date: "2022-01-17"
+author: "BIGBEASTISHANK"
+read_time: "5 min"
+---
+
+# Maths
+- Chapter 4
+- Chapter 5
+- Chapter 9
+- Chapter 10
+- Chapter 11
+- Chapter 13
+- Chapter 14
+
+# Science
+- Chapter 4
+- Chapter 5
+- Chapter 8
+- Chapter 9
+- Chapter 12
+- Chapter 13
+- Chapter 15
+
+# Social Science
+
+- History
+  - Chapter 2
+
+- Geography
+  - Chapter 5
+  - Chapter 6
+
+- Civice
+  - Chapter 6
+  - Chapter 7
+
+- Economics
+  - Chapter 3
+  - Chapter 4
+
+# English
+
+- First Flight
+  - Chapter 7
+  - Chapter 9
+
+- Poem
+  - Porm 4
+  - Poem 6
+  - Poem 9
+
+- Footprints Without Feet
+  - Chapter 6
+  - Chapter 7
+  - Chapter 8
+  - Chapter 9
+
+# Computer (Code 402)
+
+- Employblity Skills
+  - Unit 4
+  - Unit 5
+
+- Subject Specific Skills
+  - Unit 3
+  - Unit 4
+
+##
+##
+
+**Note: I don't have notes of Hindi B**
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -12,58 +12,60 @@ export default function Blog({ allPostsData }) {
       </Head>
 
       <Box className="blog">
+        <Heading
+          className="heading"
+          fontSize={{ lg: "6xl", md: "5xl", sm: "39px", base: "25px" }}
+          fontWeight="700"
+          textAlign={"center"}
+          mb={{ lg: "20px", md: "15px" }}
+        >
+          <Text className="head">My Blog</Text>
+        </Heading>
+
+        <Box
+          className="content"
+          fontSize={{
+            xl: "25px",
+            lg: "20px",
+            md: "18px",
+            sm: "16px",
+            base: "13px",
+          }}
+          fontWeight="700"
+          textAlign={"center"}
+        >
+          <Text>
+            I write my blogs over here. You can see all the post by just
+            clicking on title of the blogs. I hope you enjoy
+          </Text>
+        </Box>
+
+        <Skeleton
+          startColor="pink.500"
+          endColor="orange.500"
+          ml={"-30px"}
+          mr={"-30px"}
+          w={"810"}
+          h={"2px"}
+          mb={6}
+          mt={6}
+        />
+
         {allPostsData.map(({ id, date, title, description }) => (
           <li key={id}>
-            <Heading
-              className="heading"
-              fontSize={{ lg: "6xl", md: "5xl", sm: "39px", base: "25px" }}
-              fontWeight="700"
-              textAlign={"center"}
-              mb={{ lg: "20px", md: "15px" }}
-            >
-              <Text className="head">My Blog</Text>
-            </Heading>
-
-            <Box
-              className="content"
-              fontSize={{
-                xl: "25px",
-                lg: "20px",
-                md: "18px",
-                sm: "16px",
-                base: "13px",
-              }}
-              fontWeight="700"
-              textAlign={"center"}
-            >
-              <Text>
-                I write my blogs over here. You can see all the post by just
-                clicking on title of the blogs. I hope you enjoy
-              </Text>
-            </Box>
-
-            <Skeleton
-              startColor="pink.500"
-              endColor="orange.500"
-              ml={"-30px"}
-              mr={"-30px"}
-              w={"810"}
-              h={"2px"}
-              mb={6}
-              mt={6}
-            />
-
             {/********************************/}
 
-            <Box mb={5} className="posts">
+            <Box mb={50} className="posts">
               <Heading
-                fontSize={{ lg: "4xl", md: "3xl", sm: "28px", base: "18px" }}
+                fontSize={{ lg: "3xl", md: "2xl", sm: "24px", base: "16px" }}
                 fontWeight="700"
                 mb={{ lg: "10px", base: "5px" }}
                 mt={"15px"}
               >
                 <NextLink href={`/blog/${id}`}>
-                  <Text className='title' cursor={'pointer'}>{title}</Text>
+                  <Text className="title" cursor={"pointer"}>
+                    {title}
+                  </Text>
                 </NextLink>
               </Heading>
 
@@ -72,9 +74,8 @@ export default function Blog({ allPostsData }) {
               <Text
                 className="description"
                 fontSize={{
-                  xl: "23px",
-                  lg: "18px",
-                  md: "16px",
+                  xl: "19px",
+                  md: "15px",
                   sm: "14px",
                   base: "11px",
                 }}
@@ -87,8 +88,7 @@ export default function Blog({ allPostsData }) {
               <Text
                 className="date"
                 fontSize={{
-                  xl: "19px",
-                  lg: "14px",
+                  xl: "16px",
                   md: "12px",
                   sm: "10px",
                   base: "7px",
@@ -99,10 +99,9 @@ export default function Blog({ allPostsData }) {
 
               {/**********/}
             </Box>
-
-            {/********************************/}
           </li>
         ))}
+        {/********************************/}
       </Box>
     </>
   );
