1242d1c69e19d10f50500d3cb194e3d437e934be
BIGBEASTISHANK <pranjal3211p@gmail.com>
2022-01-18 20:07:57 +0530

    removed unused line

--- src/components/date.jsx
+++ src/components/date.jsx
@@ -5,8 +5,7 @@ export default function Date({ dateString }) {
 
   // Variable
   const date = parseISO(dateString);
-
-  console.log(date)
+  
   // Returning Time in Correct Form
   return <time dateTime={dateString}>{format(date, "d LLLL, yyyy")}</time>;
 }
