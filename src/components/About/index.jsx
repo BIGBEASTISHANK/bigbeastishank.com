21d57bb13ffabb5ddc908d0d746f003b04506701
BIGBEASTISHANK <pranjal3211p@gmail.com>
2023-02-12 15:58:26 +0530

    changed 'My tools' sections

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,130 +1,46 @@
-import {
-  Box,
-  Wrap,
-  Text,
-  Heading,
-  Divider,
-  WrapItem,
-  useColorMode,
-  useMediaQuery,
-  Skeleton,
-} from "@chakra-ui/react";
-import {
-  faCss3,
-  faDiscord,
-  faHtml5,
-  faJs,
-  faLinux,
-  faOpera,
-  faPython,
-  faSass,
-  faSpotify,
-  faUnity,
-  faWindows,
-  faAndroid,
-} from "@fortawesome/free-brands-svg-icons";
-import { faC, faCode, faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Importing Stuffs
+import { Box, Heading, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
 
 export default function About() {
   // Variables
-  const [isSmallerThen590] = useMediaQuery("(max-width: 590px)");
   const { colorMode } = useColorMode();
-
-  const Tools = [
-    {
-      name: "Html",
-      icon: faHtml5,
-    },
-    {
-      name: "CSS",
-      icon: faCss3,
-    },
-    {
-      name: "JavaScript",
-      icon: faJs,
-    },
-    {
-      name: "Python",
-      icon: faPython,
-    },
-    {
-      name: "SASS",
-      icon: faSass,
-    },
-    {
-      name: "C#",
-      icon: faC,
-    },
-    {
-      name: "VSCodium",
-      icon: faCode,
-    },
-    {
-      name: "Spotify",
-      icon: faSpotify,
-    },
-    {
-      name: "Brave",
-      icon: faGlobe,
-    },
-    {
-      name: "Unity",
-      icon: faUnity,
-    },
-    {
-      name: "Discord",
-      icon: faDiscord,
-    },
-    {
-      name: "Linux",
-      icon: faLinux,
-    },
-    {
-      name: "Android",
-      icon: faAndroid,
-    },
-  ];
-
+  
+  // Returning Html
   return (
     <>
-      <Box>
+      {/* Hero Header */}
+      <Box className="hero">
         <Heading
           fontSize={{ lg: "6xl", md: "5xl", sm: "39px", base: "25px" }}
           fontWeight="700"
           textAlign={"center"}
           mb={{ lg: "20px", md: "15px" }}
         >
-          <Text className="heading" color={"primary"}>
-            My Tools
+          <Text className="hero-heading" color={"primary"}>
+            Tools & Languages I use
           </Text>
         </Heading>
-        <Wrap spacing="20px" justify="center">
-          {Tools.map((item) => (
-            <WrapItem key={item.name}>
-              <Box bg={"rgba( 0, 0, 0, 0.085 )"} p={"10px"} borderRadius="5px">
-                <FontAwesomeIcon
-                  icon={item.icon}
-                  fontSize={isSmallerThen590 ? "50px" : "75px"}
-                />
-                <Skeleton
-                  h={"2px"}
-                  endColor={colorMode === "dark" ? "light" : "dark"}
-                  startColor={colorMode === "dark" ? "light" : "dark"}
-                  mb={"15px"}
-                  mt={"15px"}
-                />
-                <Text
-                  textAlign="center"
-                  fontSize={isSmallerThen590 ? "11px" : ""}
-                  userSelect={"none"}
-                >
-                  {item.name}
-                </Text>
-              </Box>
-            </WrapItem>
-          ))}
-        </Wrap>
+
+        {/* Small summery about me */}
+        <Box
+          className="hero-content"
+          fontSize={{
+            xl: "25px",
+            lg: "20px",
+            md: "18px",
+            sm: "16px",
+            base: "13px",
+          }}
+          fontWeight="700"
+        >
+          <Text textAlign={"center"}>
+            The programming languages I know are: HTML, CSS, JavaScript, Python, Sass, C#.
+          </Text>
+          <Text textAlign={"center"}>
+            The Softwares I use in my daily projects are: VS Codium, Unity, Brave, Spotify, Discord.
+          </Text>
+        </Box>
       </Box>
     </>
   );
