3cd52fdfe1d02125b89e9be9c2f76f0abab922bc
BIGBEASTISHANK <pranjal3211p@gmail.com>
2023-02-21 01:05:58 +0530

    Updated analytics and adsense

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -11,10 +11,36 @@ class MyDocument extends Document {
 
   render() {
     return (
-
       // Rendring basic html format, if not then I don't know💀
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Google ads start */}
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5212436761863059"
+            crossorigin="anonymous"
+          ></script>
+          {/* Google ads end */}
+
+          {/* Google analytics code start */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+          {/* Google analytics code end */}
+        </Head>
 
         <body>
           <Main />
