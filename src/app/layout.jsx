e141df31beb1a17a220cd449183c24c987af0581
BIGBEASTISHANK <antihack@bigbeastishank.com>
2023-12-27 17:56:28 +0530

    added: googel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,8 @@ import NavBar from "@/components/Sections/NavBar";
 import ActiveSectionProvider from "@/components/Context/ActiveSection";
 import Footer from "@/components/Sections/Footer";
 import ThemeSwitcher from "@/components/Subs/ThemeSwitcher";
+// import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata = {
   title: "Welcome to BIGBEASTISHANK's Portfolio website",
@@ -21,16 +23,33 @@ export default function RootLayout({ children }) {
         <div className="bg-[#dbd7fd] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
         {/* Initial 2 background color end */}
 
-          <ActiveSectionProvider>
-            <NavBar />
-            {children}
-            <Footer />
-          </ActiveSectionProvider>
+        <ActiveSectionProvider>
+          <NavBar />
+          {children}
+          <Footer />
+        </ActiveSectionProvider>
 
-          {/* Fixed Display */}
-          <ThemeSwitcher />
+        {/* Fixed Display */}
+        <ThemeSwitcher />
         <Toaster position="top-right" reverseOrder={false} />
       </body>
+
+      {/* Currently Google analytics is not working for me (https://github.com/vercel/next.js/discussions/59962)! */}
+      {/* <GoogleAnalytics gaId={process.env.gtag} /> */}
+
+      {/* Currently using this insted of {GoogleAnalytics} */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.gtag}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.gtag}');
+        `}
+      </Script>
     </html>
   );
 }
