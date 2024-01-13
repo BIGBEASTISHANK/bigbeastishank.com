6cfb956b14deebc15a7b71df158dde388873b3db
BIGBEASTISHANK <antihack@bigbeastishank.com>
2024-01-14 00:39:30 +0530

    update: seo improve

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ export default function Hero() {
       </div>
 
       {/* Button */}
-      <Link href={"/contact"}>
+      <Link href={"/"}>
         <motion.button
           initial={{ opacity: 0, scale: 0.4, y: 50 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
