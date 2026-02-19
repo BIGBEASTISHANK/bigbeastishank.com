be5b0e02628e149dca2accfd67cbfe685d4c8e57
BIGBEASTISHANK <ishank@bigbeastishank.com>
2026-02-19 12:05:27 +0530

    [UPDATE] added rss button in footer and blog page desc

--- src/components/(1) Layout/Footer.tsx
+++ src/components/(1) Layout/Footer.tsx
@@ -5,90 +5,104 @@ import { FullDivider, ShortDivider } from "@/utility/Dividers";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { FaRss } from "react-icons/fa";
 
 const forbidenPathsForVisitorCount = ["/admin"];
 
 export default function Footer() {
-    const pathName: string = usePathname().split("/blogs/")[1];
-    const currentPath = usePathname();
-    const [nthvisitor, setNthvisitor] = useState<number>();
+  const pathName: string = usePathname().split("/blogs/")[1];
+  const currentPath = usePathname();
+  const [nthvisitor, setNthvisitor] = useState<number>();
 
-    useEffect(() => {
-        const isForbiddenPath = forbidenPathsForVisitorCount.some(path => 
-            currentPath.startsWith(path)
-        );
+  useEffect(() => {
+    const isForbiddenPath = forbidenPathsForVisitorCount.some((path) =>
+      currentPath.startsWith(path),
+    );
 
-        async function handleVisitorCount() {
-            try {
-                const method = isForbiddenPath ? "GET" : "PUT";
-                const response = await fetch("/api/visitorCount", {
-                    method: method,
-                });
+    async function handleVisitorCount() {
+      try {
+        const method = isForbiddenPath ? "GET" : "PUT";
+        const response = await fetch("/api/visitorCount", {
+          method: method,
+        });
 
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-
-                const data = await response.json();
-                setNthvisitor(data.nthvisitor);
-            } catch (error) {
-                console.error("Failed to fetch visitor count:", error);
-            }
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        handleVisitorCount();
-    }, []);
+        const data = await response.json();
+        setNthvisitor(data.nthvisitor);
+      } catch (error) {
+        console.error("Failed to fetch visitor count:", error);
+      }
+    }
 
-    return (
-        <footer
-            className={`text-center justify-center items-center my-5 ${
-                pathName != null
-                    ? "max-w-[110rem] sm:p-5 p-3"
-                    : "md:max-w-[45rem] max-w-[35rem]"
-            } mx-auto`}
-        >
-            <FullDivider />
+    handleVisitorCount();
+  }, []);
 
-            <div className="flex flex-col border-1 border-gray-600 rounded-3xl backdrop-blur-md sm:p-5 p-3 justify-center items-center">
-                {nthvisitor && (
-                    <motion.p
-                        initial={{ scale: 0 }}
-                        animate={{ scale: 1 }}
-                        className="px-3 py-2 border border-[#0088CC] shadow-sm shadow-[#0088CC] rounded-3xl text-center text-sm text-[#0088CC] w-fit mb-3 select-none"
-                    >
-                        You are {nthvisitor.toLocaleString('en-IN')}th Visitor.
-                    </motion.p>
-                )}
-                
-                <p className="md:text-base text-sm">
-                    Made with ❤️ by{" "}
-                    <strong>
-                        <a
-                            href="/github"
-                            target="_blank"
-                            className="text-[#0088CC] hover:underline outline-none"
-                        >
-                            BIGBEASTISHANK
-                        </a>
-                    </strong>
-                </p>
-                
-                <ShortDivider customCSS="my-2" />
-                
-                <div className="mt-3 md:text-2xl text-xl flex flex-wrap gap-5 justify-center mx-10">
-                    {socialMedia.map((data, index) => (
-                        <Link
-                            key={index}
-                            aria-label={data.label}
-                            href={data.link}
-                            target="_blank"
-                            className={"outline-none"}
-                        >
-                            <data.icon />
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer
+      className={`text-center justify-center items-center my-5 ${
+        pathName != null
+          ? "max-w-[110rem] sm:p-5 p-3"
+          : "md:max-w-[45rem] max-w-[35rem]"
+      } mx-auto`}
+    >
+      <FullDivider />
+
+      <div className="flex flex-col border-1 border-gray-600 rounded-3xl backdrop-blur-md sm:p-5 p-3 justify-center items-center">
+        {nthvisitor && (
+          <motion.p
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            className="px-3 py-2 border border-[#0088CC] shadow-sm shadow-[#0088CC] rounded-3xl text-center text-sm text-[#0088CC] w-fit mb-3 select-none"
+          >
+            You are {nthvisitor.toLocaleString("en-IN")}th Visitor.
+          </motion.p>
+        )}
+
+        <p className="md:text-base text-sm">
+          Made with ❤️ by{" "}
+          <strong>
+            <a
+              href="/github"
+              target="_blank"
+              className="text-[#0088CC] hover:underline outline-none"
+            >
+              BIGBEASTISHANK
+            </a>
+          </strong>
+        </p>
+
+        <ShortDivider customCSS="my-2" />
+
+        <div className="mt-3 md:text-2xl text-xl flex flex-wrap gap-5 justify-center items-center mx-10">
+          {/* Social media link */}
+          {socialMedia.map((data, index) => (
+            <Link
+              key={index}
+              aria-label={data.label}
+              href={data.link}
+              target="_blank"
+              className={"outline-none"}
+            >
+              <data.icon />
+            </Link>
+          ))}
+
+          {/* Divider */}
+          <p className="text-[#1793D1] select-none">|</p>
+
+          {/* RSS */}
+          <Link
+            aria-label="rss"
+            href="/rss.xml"
+            className="outline-none flex items-center gap-2 text-[1.3rem]"
+          >
+            <FaRss />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/utility/Dividers.tsx
+++ src/utility/Dividers.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { motion } from "framer-motion";
 
-export function ShortDivider({ delay = 0, customCSS = "my-7" }: { delay?: number, customCSS?: string }) {
+// Short Divider
+export function ShortDivider({
+  delay = 0,
+  customCSS = "my-7",
+}: {
+  delay?: number;
+  customCSS?: string;
+}) {
   return (
     <motion.div
       initial={{ width: "0%" }}
@@ -12,14 +19,20 @@ export function ShortDivider({ delay = 0, customCSS = "my-7" }: { delay?: number
   );
 }
 
-export function FullDivider({ delay = 0, customCSS = "my-12" }: { delay?: number, customCSS?: string }) {
-    return (
-      <motion.div
-        initial={{ width: "0%" }}
-        animate={{ width: "100%" }}
-        transition={{ delay: delay, duration: 0.5, type: "spring" }}
-        className={`h-[.125rem] bg-[#1793D1] z-10 ${customCSS} mx-auto`}
-      />
-    );
-  }
-  
+// Full Divider
+export function FullDivider({
+  delay = 0,
+  customCSS = "my-12",
+}: {
+  delay?: number;
+  customCSS?: string;
+}) {
+  return (
+    <motion.div
+      initial={{ width: "0%" }}
+      animate={{ width: "100%" }}
+      transition={{ delay: delay, duration: 0.5, type: "spring" }}
+      className={`h-[.125rem] bg-[#1793D1] z-10 ${customCSS} mx-auto`}
+    />
+  );
+}
