ed62f0ba698df0e655aad5a3789bbdd63df56139
BIGBEASTISHANK <ishank@bigbeastishank.com>
2026-06-02 16:36:02 +0530

    [FIX] Stickey scroll of TOC

--- src/components/(7) Blogs/posts/UseClientIndex.tsx
+++ src/components/(7) Blogs/posts/UseClientIndex.tsx
@@ -206,7 +206,7 @@ export default function UseClientIndex({
     TOCData: { level: number; content: string }[];
 }) {
     return (
-        <div className="flex min-[1531px]:px-20 px-0 scroll-mt-28 overflow-hidden">
+        <div className="flex min-[1531px]:px-20 px-0 scroll-mt-28">
             {/* Main content */}
             <div
                 id="blogPost"
