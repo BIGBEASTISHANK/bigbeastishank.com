0b786b00318468ccab81fc1a8689872660b9a916
BIGBEASTISHANK <ishank@bigbeastishank.com>
2026-06-04 18:28:45 +0530

    [UPDATE] Prep for preview deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-unused-vars": "off",
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-explicit-any": "off",
+	" @typescript-eslint/no-wrapper-object-types": "off",
     },
   },
 ];
